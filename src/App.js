2940cf5de436c7c76392f4ed011ecee49f51df4d
Mayumi <Kagura004@gmail.com>
2020-12-18 23:15:44 +0900

    implement action and action creater

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ const App = () => (<Counter></Counter>)
 class Counter extends Component {
   constructor(props) {
     super(props)
-    console.log(this.state);
     this.state = { count: 0 }
   }
 
---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMRNT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => ({
+    type: DECREMENT
+})
